f35b06fd526f66415ac30ebd6d11716b25e7740a
AlessandroDLuna <alex14god2c@gmail.com>
2024-10-17 14:55:20 -0600

    Código Inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+from classWindow import Window
+
+ventana = Window()
+ventana.setDimensions(500,500)
+ventana.settitulo("Pour Mon Petit Lune")
+ventana.setResize(True)
+
+#print (ventana.getDimensions())
+#print(ventana.getResize())
+#print(ventana.gettitulo())
+
+ventana.crear_ventana()
